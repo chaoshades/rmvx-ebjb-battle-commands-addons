6b38eff8346fd3eee53345165f81935d91d026cf
chaoshades <chaos_hades@hotmail.com>
2022-01-16 17:48:55 -0500

    FIX - code readability

--- addons/Morph-Revert/build.rb
+++ addons/Morph-Revert/build.rb
@@ -5,18 +5,18 @@ module EBJB_BCMorphRevert
   TARGETS = [
 	"src/Script_Header.rb",
     "src/BattleCommands_Config.rb",
-	"src/BattleSystem_Config.rb",
+    "src/BattleSystem_Config.rb",
     "src/Game Objects/Game_Actor.rb",
-	"src/Game Objects/Game_BattleAction.rb",
-	"src/Game Objects/Game_Battle_Commands.rb",
-	"src/Game Objects/Game_Party.rb",
+    "src/Game Objects/Game_BattleAction.rb",
+    "src/Game Objects/Game_Battle_Commands.rb",
+    "src/Game Objects/Game_Party.rb",
     "src/Misc Objects/UsableItemFilter.rb",
     "src/Scenes/Scene_BattleCommands.rb",
-	"src/Scenes/Scene_Battle.rb",
-	"src/Sprites Objects/Spriteset_Battle.rb",
+    "src/Scenes/Scene_Battle.rb",
+    "src/Sprites Objects/Spriteset_Battle.rb",
     "src/User Interface/Vocab.rb",
-	"src/Windows/Window_ActorCommand.rb",
-	"src/Windows/Window_AutoBattle_Command.rb",
+    "src/Windows/Window_ActorCommand.rb",
+    "src/Windows/Window_AutoBattle_Command.rb",
   ]
 end
 
